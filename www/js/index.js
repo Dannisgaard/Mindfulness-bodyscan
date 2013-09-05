ebedea127df8bda53939150718e4d31dde9d5d50
Dannisgaard <Lars.Radoor.Hansen@gmail.com>
2013-09-05 21:55:49 +0200

    Test buttons

--- www/js/index.js
+++ www/js/index.js
@@ -22,12 +22,12 @@ $(function(){
 		$(this).removeClass('tapped');
 	});
 	
-	$('a').on('tap', function(e){
+	/*$('a').on('tap', function(e){
 		e.preventDefault();
 	
 		//do your thing
 		$('#output').append('<li>You tapped a button.</li>')
 	
 		return false;
-	});
+	});*/
 });
